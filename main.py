0590642c00955e43a4a09d745c69db19f05fb60e
Jirapawee-R <42169410+Jirapawee-R@users.noreply.github.com>
2021-08-10 19:37:22 +0700

    Define Variables

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import pulp as p
 
 # Set up a maximization problem and its variable
 # (I) Name the problem as "IceCream_maxProfit_Analysis" using objective of minimizing cost :
-Lp_prob = p.LpProblem('IceCream_maxProfit_Analysis', p.LpMinimize)
+profit = p.LpProblem('IceCream_maxProfit_Analysis', p.LpMinimize)
+
+# (II) Set up problem variables :
+# Box of vanilla ice-cream
+vanilla = p.LpVariable('v', lowBound=0)
+# Box of strawberry ice-cream
+strawberry = p.LpVariable('s', lowBound=0)
+
+
 
 
 
