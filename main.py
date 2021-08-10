9814df13bf5a1b166f4de9f9cba3d05b5647cded
Jirapawee-R <42169410+Jirapawee-R@users.noreply.github.com>
2021-08-10 20:16:16 +0700

    solve problem and show the final result

--- main.py
+++ main.py
@@ -18,8 +18,16 @@ profit += (2*vanilla) + (3*strawberry)
 # (I) Amount of daily fresh milk order
 profit += (0.5*vanilla) + (0.2*strawberry) <= 10
 # (II) Number of doll for promoting in a day
-profit += vanilla + strawberry <= 30
+profit += vanilla + strawberry >= 30
 
+# State problem
+print(profit)
 
+# Solve the problem
+profit.solve()
 
-
+# Result
+print('Result :\n--------------------------------------------------------')
+print('Maximum Profit :', p.value(profit.objective))
+print('Optimal Number of Box(es) of Vanilla Ice-cream : ', p.value(vanilla))
+print('Optimal Number of Box(es) of Strawberry Ice-cream : ', p.value(strawberry))
