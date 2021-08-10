3ee08553738f169faac5a18a9f169f0adad32a74
Jirapawee-R <42169410+Jirapawee-R@users.noreply.github.com>
2021-08-10 19:14:01 +0700

    Initial : import library and state model

---
+++ main.py
@@ -0,0 +1,11 @@
+# Import library PuLP as p
+import pulp as p
+
+# Set up a maximization problem and its variable
+# (I) Name the problem as "IceCream_maxProfit_Analysis" using objective of minimizing cost :
+Lp_prob = p.LpProblem('IceCream_maxProfit_Analysis', p.LpMinimize)
+
+
+
+
+
